de687b4f90e8f4dd84bb2b0c2ee65453843373ba
Pedro D. Bello-Maldonado <metalcycling@gmail.com>
2023-02-12 16:41:31 -0500

    Updated power measuring to be more efficient

--- power.sh
+++ power.sh
@@ -4,17 +4,21 @@ function measure()
 {
     timestamp=$(date "+%F-%T-%Z")
     num_gpus=$(nvidia-smi -L | wc -l)
+    gpus_power_data=$(nvidia-smi --query --display=POWER)
+
+    mapfile -t gpus_power_draw <<< $(echo "${gpus_power_data}" | grep "Power Draw")
+    mapfile -t gpus_power_limit <<< $(echo "${gpus_power_data}" | grep "  Power Limit")
 
     echo -n ${timestamp} ""
 
     for gpu_id in $(seq 0 $((${num_gpus} - 1)))
     do
-        power_draw=$(nvidia-smi -i ${gpu_id} -q | grep "Power Draw" | awk '{print $4}')
-        power_limit=$(nvidia-smi -i ${gpu_id} -q | grep "  Power Limit" | awk '{print $4}')
-        echo -n ${power_draw}/${power_limit}
+        power_draw=$(echo ${gpus_power_draw[${gpu_id}]} | awk '{print $4}')
+        power_limit=$(echo ${gpus_power_limit[${gpu_id}]} | awk '{print $4}')
+        echo -n ${power_draw}/${power_limit} ""
     done
 
-    echo " "
+    echo
 }
 
 while true
