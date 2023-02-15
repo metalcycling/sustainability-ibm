1147c6446a01c586bbb7c9a257fd502a9cb2204d
Pedro D. Bello-Maldonado <metalcycling@gmail.com>
2023-02-15 15:26:49 -0500

    Updated power measuaring for faster sampling

--- power.sh
+++ power.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
-function measure()
-{
-    timestamp=$(date "+%F-%T-%Z")
-    num_gpus=$(nvidia-smi -L | wc -l)
-    gpus_power_data=$(nvidia-smi --query --display=POWER)
-
-    mapfile -t gpus_power_draw <<< $(echo "${gpus_power_data}" | grep "Power Draw")
-    mapfile -t gpus_power_limit <<< $(echo "${gpus_power_data}" | grep "  Power Limit")
-
-    echo -n ${timestamp} ""
-
-    for gpu_id in $(seq 0 $((${num_gpus} - 1)))
-    do
-        power_draw=$(echo ${gpus_power_draw[${gpu_id}]} | awk '{print $4}')
-        power_limit=$(echo ${gpus_power_limit[${gpu_id}]} | awk '{print $4}')
-        echo -n ${power_draw}/${power_limit} ""
-    done
-
-    echo
-}
-
 while true
 do
-    measure
+    echo $(date "+%F-%T.%3N-%Z") $(nvidia-smi --query-gpu=power.draw --format=csv,noheader,nounits)
     sleep ${1}
 done
